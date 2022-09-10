de5cc7ab0c5ac2d876e43ba6ee4c27115c657ce3
qualterz <qualterz@tutamail.com>
2022-09-10 13:27:06 +0300

    Replace default instance with custom

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = "https://localhost:7297"
+export const priceobserver = axios.create({
+    baseURL: "https://localhost:7297"
+})
 
 export const zakazua = axios.create({
     baseURL: "https://stores-api.zakaz.ua"
