bb0c607adcc569b19e429bbca2e9348dbea43823
qualterz <qualterz@tutamail.com>
2022-09-17 19:32:46 +0300

    Set empty alt text

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -92,7 +92,7 @@ export default function ProductList() {
           <div key={product.ean} className="list-group-item">
             <div className="row align-items-center">
               <div className="col-auto">
-                <img className="avatar" src={product.img.s150x150} />
+                <img className="avatar" src={product.img.s150x150} alt=""/>
               </div>
 
               <div className="col">{product.title}</div>
