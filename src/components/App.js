f86a88dd54917405ea021f473344afa64fb55d48
qualterz <qualterz@tutamail.com>
2022-09-19 17:10:47 +0300

    Setup root route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import { Route } from "react-router-dom"
+import { Routes } from "react-router-dom"
+
 import ProductList from "./ProductList"
 
 function App() {
+  const WrappedProductList = () =>
+    <div className="row">
+      <div className="col" style={{ maxHeight: "100vh" }}>
+        <ProductList />
+      </div>
+    </div>
+
   return (
     <div className="container-xxl">
-      <div className="row">
-        <div className="col" style={{maxHeight: "100vh"}}>
-          <ProductList />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<WrappedProductList />}/>
+      </Routes>
     </div>
   )
 }
